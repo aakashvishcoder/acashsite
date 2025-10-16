5621292dd9833446109d5955da45b5a27502cf95
Aakash Vishnuvarth <aakashvish07@gmail.com>
2025-10-16 08:46:44 -0500

    manually initialized tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./src/**/*.{js, jsx, ts, tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                orbitron: ['"Orbitron"', 'sans-serif'],
+                rajdhani: ['"Rajdhani"', 'sans-serif']
+            },
+        }
+    },
+    plugins: [],
+};
